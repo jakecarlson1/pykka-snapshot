f5e8db8480b1af13c7fc048f88535d3d87cea374
Jake Carlson <jrcarlson@smu.edu>
2018-10-17 15:13:28 -0500

    Fix argument type in command line args

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from examples import run_example
 def build_arg_parser():
     parser = argparse.ArgumentParser(prog='snapshot-threads')
     parser.add_argument('-e', '--example', choices=[1], default=1,
-                        help='Example to run', metavar='num')
+                        type=int, help='Example to run', metavar='num')
 
     # TODO: add num incrementors and send prop as command line args
     
